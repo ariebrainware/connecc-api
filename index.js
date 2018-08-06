11823c468969c3de7eae7e31f14a927ee4f2dfcc
Arie Brainware <ariebrainware@gmail.com>
2018-08-06 15:11:24 +0700

    Fix error file path

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require(`express`)
+const bodyParser = require(`body-parser`)
+const cors = require(`cors`)
+const contact = require(`./lib/api/index.js`)
+
+const server = express()
+const port = 1118
+
+server.use(cors())
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+server.get(`/`, contact.show)
+
+server.listen(port, () => console.log(`
+        Contact server listening on port ${port}
+        `))
