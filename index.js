d78e2c457bba1ea2a5770153e52e97b43cfd64cb
Arie Brainware <ariebrainware@gmail.com>
2018-08-08 01:47:36 +0700

    Add /devteam endpoint for about Developer page.
    and separate object devteam and contacts.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const cors = require(`cors`);
 
-const contact = require(`./api/index.js`);
+const extModule = require(`./api/index.js`);
+const devTeam = extModule.devTeam
+const contact = extModule.contacts
 
 const server = express();
 const port = 3000;
@@ -16,10 +18,10 @@ server.use(
 );
 
 server.get(`/contacts`, contact.showContact);
+server.get(`/contacts/search`, contact.searchContact);
 server.post(`/contacts`, contact.addContact);
 server.delete(`/contacts/:id`, contact.deleteContact);
-server.get(`/contact/search`, contact.searchContact);
-
+server.get(`/devteam`, devTeam.showDevTeam);
 server.listen(port, () =>
   console.log(`
         Contact server listening on port ${port}
