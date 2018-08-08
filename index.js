bc3a9c2927e92a5ec6792b75b295d1b38c049fd2
Arie Brainware <ariebrainware@gmail.com>
2018-08-08 15:44:14 +0700

    Move endpoint to show DevTeam from /devteam to /connecc/devteam

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ server.get(`/contacts`, contact.showContact);
 server.get(`/contacts/search`, contact.searchContact);
 server.post(`/contacts`, contact.addContact);
 server.delete(`/contacts/:id`, contact.deleteContact);
-server.get(`/devteam`, devTeam.showDevTeam);
+server.get(`/connecc/devteam`, devTeam.showDevTeam);
 
 server.listen(port, () =>
   console.log(`
