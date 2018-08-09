092060c5178ee69c68a58d1929bf06242cff9876
Arie Brainware <ariebrainware@gmail.com>
2018-08-09 21:52:30 +0700

    Add create, delete, and update endpoint

--- api/users/index.js
+++ api/users/index.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const controller = require('./controller')
 
 /* ROUTES/VIEW */
-router.get('/', controller.show);
-router.post('/', controller.add);
-router.get('/:id', controller.searchByID);
-
+router.get('/', controller.showUser);
+router.post('/', controller.addUser);
+router.get('/:id', controller.searchUserByID);
+router.delete('/:id', controller.deleteUser)
+router.put('/:id',controller.updateUser)
 
 module.exports = router;
