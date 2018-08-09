4063b7859a95b60632d7694c50931785f79cf108
Arie Brainware <ariebrainware@gmail.com>
2018-08-09 21:51:52 +0700

    Finishing CRUD and Search function

--- api/users/controller.js
+++ api/users/controller.js
@@ -1,7 +1,8 @@
 const models = require('../../models')
 const User = models.User
+
 const controller = {
-    show: (req, res, next) => {
+    showUser: (req, res, next) => {
         User
             .findAll().then(users => {
                 res.send({
@@ -14,15 +15,15 @@ const controller = {
             })
     },
 
-    add: (req, res, next) => {
-        if(req.body.username && req.body.password && req.body.email){
+    addUser: (req, res, next) => {
+        if (req.body.username && req.body.password && req.body.email) {
             User
-                .build({                    
+                .build({
                     username: req.body.username,
                     password: req.body.password,
                     email: req.body.email,
                     createdAt: new Date(),
-                    UpdateAt: new Date()
+                    updatedAt: new Date()
                 })
                 .save()
                 .then(user => {
@@ -32,10 +33,46 @@ const controller = {
                         message: err
                     })
                 })
+        } else {
+            res.status(404).send({
+                message: 'Please fill the input field'
+            })
         }
     },
+    deleteUser: (req, res, next) => {
+        const id = Number(req.params.id)
+        User
+            .destroy({
+                where: {
+                    id: id
+                }
+            }).then(
+                res.status(200).send('Data successfully deleted')
+            )
 
-    searchByID: (req, res, next) => {
+    },
+    updateUser: (req, res, next) => {
+        const id = Number(req.params.id)
+        if (req.body.password && req.body.email) {
+            User
+                .update({
+                    password: req.body.password,
+                    email: req.body.email,
+                    updatedAt: new Date()
+                }, {where:{id: id}})
+                .then(() => {
+                    res.status(200).send({
+                        message: "Updating success"
+                    })
+                })
+        } else {
+            res.status(417).send({
+                message: "Please specify password field and email field!"
+            })
+        }
+
+    },
+    searchUserByID: (req, res, next) => {
         const id = Number(req.params.id)
         User
             .findById(id)
