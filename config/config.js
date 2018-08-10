b5de17ff51756f6b97c1ecca6a00768e448f2f8f
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-10 10:00:20 +0700

    Add dotenv-extended module definition for .env variable

--- config/config.js
+++ config/config.js
@@ -1,26 +1,35 @@
+require('dotenv-extended').load({
+  encoding: 'utf8',
+  silent: true,
+  path: '.env',
+  defaults: '.env.defaults',
+  schema: '.env.schema',
+  errorOnMissing: false,
+  errorOnExtra: false,
+  assignToProcessEnv: true,
+  overrideProcessEnv: false
+});
+
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
     "dialect": process.env.DB_DIALECT
   },
   "test": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
     "dialect": process.env.DB_DIALECT
   }
 }
