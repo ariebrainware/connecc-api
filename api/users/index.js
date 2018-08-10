1f2986de5bc1fa864e5bf790166662cca5003bc2
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-10 09:59:33 +0700

    Add CRUD and Search endpoint

--- api/users/index.js
+++ api/users/index.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const controller = require('./controller')
 
 /* ROUTES/VIEW */
-router.get('/', controller.show);
+router.get('/', controller.showUser);
+router.post('/', controller.addUser);
+router.get('/:id', controller.searchUserByID);
+router.delete('/:id', controller.deleteUser)
+router.put('/:id',controller.updateUser)
+
 
 module.exports = router;
