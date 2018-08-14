2ee7fcb62437b627d35948f7dc3400cdeb268bf3
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-14 09:03:23 +0700

    Add variable rootUrl

--- api/index.js
+++ api/index.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const rootUrl = "https://connecc-api.herokuapp.com"
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.send({
     title: 'CONNECC-API Homepage',
     content: [{
-      devteam: "https://connecc-api.herokuapp.com/devteam/",
-      user: "https://connecc-api.herokuapp.com/users/",
-      contacts: "https://connecc-api.herokuapp.com/contacts/"
+      devteam: {
+        "show-devteam": `${rootUrl}/devteam/`,
+
+      },
+      user: {        
+        "show-users": `${rootUrl}/users/`
+      },
+      contacts: {
+        "search_contact_by_name": `${rootUrl}/search?q={name}`,
+        "search_contact_by_id": `${rootUrl}/search/:id`,
+        "show-contacts":`${rootUrl}/contacts/`
+      }
     }]
   });
 });
