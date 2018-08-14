9f9d6e96d3ba7bc8cdb797cb575ed191f953831c
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-14 09:14:01 +0700

    Remove unnecessary message

--- api/contacts/index.js
+++ api/contacts/index.js
@@ -4,18 +4,11 @@ const router = express.Router();
 const controller = require('./controller')
 
 /* ROUTES/VIEW */
-router.get('/', async(req,res,next)=>{
-    let data = await controller.showContacts()
-    res.send({
-        "search_contact_by_name": "https://connecc-api.herokuapp.com/search?q={name}",
-        "search_contact_by_id": "https://connecc-api.herokuapp.com/search/:id",
-        data
-    })
-});
+router.get('/', controller.show)
 router.get('/search', controller.searchByKeyword);
-router.post('/', controller.addContact);
+router.post('/', controller.add);
 router.get('/:id', controller.searchByID);
-router.delete('/:id', controller.deleteContact)
-router.put('/:id',controller.updateContact)
+router.delete('/:id', controller.delete)
+router.put('/:id',controller.update)
 
 module.exports = router;
