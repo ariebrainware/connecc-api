b0b6cdafc0d573a0db0686fc99ee1e1d5feeb81b
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-15 10:27:42 +0700

    Fix devteam endpoint

--- api/devteam/index.js
+++ api/devteam/index.js
@@ -4,13 +4,7 @@ const router = express.Router();
 const controller = require('./controller')
 
 /* ROUTES/VIEW */
-router.get('/', async(req, res, next) => {
-    let data = await controller.listDevTeam();
-    res.send({
-        search: "https://connecc-api.herokuapp.com/devteam/:id",
-        data
-    })
-})
+router.get('/', controller.listDevTeam)
 router.get('/:id', controller.searchByID);
 
 module.exports = router;
