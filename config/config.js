2bfe828d718f2abc5f9150ffe8673d824c84ff7c
Arie Brainware <publicu001@gmail.com>
2019-01-16 20:02:23 +0700

    Update environment configuration

--- config/config.js
+++ config/config.js
@@ -16,20 +16,20 @@ module.exports = {
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": "mysql"
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
+    "dialect": "mysql"
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": "mysql"
   }
 }
